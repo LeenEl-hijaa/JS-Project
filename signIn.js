613cf021aaba2963feb8651ef9e6f834a0b6b80c
LeenEl-hijaa <leenkhaled77@gamil.com>
2022-04-03 16:02:27 +0300

    main

---
+++ signIn.js
@@ -0,0 +1,20 @@
+//validation for sign in form 
+
+let userName = document.getElementById("user")
+let signInPass = document.getElementById("signIn-pass")
+let signInSubmit = document.getElementById("signIn-submit")
+let userNameSpan = document.getElementById("checkEmail")
+let signInAlert = document.getElementById("signInAlert")
+signInSubmit.onclick = function () {
+    if (userName.value === localStorage.getItem("email") || userName.value === localStorage.getItem("userN")) {
+        userNameSpan.style.display = "none"
+        if (signInPass.value === localStorage.getItem("password")) {
+            signInAlert.style.display = "none"
+            window.location.href = "https://abdallah-alhasan.github.io/Quiz-site/welcome.html"
+        } else {
+            signInAlert.style.display = "inline"
+        }
+    } else {
+        userNameSpan.style.display = "inline"
+    }
+}
